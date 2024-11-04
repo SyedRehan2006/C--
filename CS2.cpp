7a5e54d2dfef381a8854f18fd6279f864b0b1a6f
Roushan Syed <roushan@healthfirsttech.com>
2024-11-04 23:46:35 +0530

    Made Player Constructor, Added buyWeapon function and displayPlayerInfo function

--- CS2.cpp
+++ CS2.cpp
@@ -3,34 +3,74 @@ using namespace std;
 class player
 {
 public:
-    void teamSelection()
+    string userName;
+    int health;
+    string team;
+    string armor;
+    string primaryWeapon;
+    string secondaryWeapon;
+    player(string userName, string team)
     {
-        cout << "Select a Team!" << endl;
-        cout << "Enter 1 for a team Terrorist " << endl;
-        cout << "Enter 2 for a team Counter-Terrorist " << endl;
-        int teamchoice;
-        cin >> teamchoice;
-        if (teamchoice == 1)
+        this->userName = userName;
+        this->health = 100;
+        this->team = team;
+        if (team == "CT")
         {
-            cout << "You have selected Team Terrorist" << endl;
-            cout << "Player Created" << endl;
+            secondaryWeapon = "USP-5";
         }
-        if (teamchoice == 2)
+        if (team == "T")
         {
-            cout << "You have selected Team Counter-Terrorist" << endl;
-            cout << "Player Created" << endl;
+            secondaryWeapon = "Glock";
         }
-        else
-        {
-            cout << "Enter a Valid number" << endl;
-            cout << "Player was not Created" << endl;
-        }
+
+        displayPlayerInfo();
     }
-    
+    void buyWeapon()
+    {
+        // string M4A1;
+        cout << "buyWeapon" << endl;
+        cout << "Press 1 for M4A1" << endl;
+        cout << "Press 2 for AUG" << endl;
+        cout << "Press 3 for AWP" << endl;
+        cout << "Press 4 for SCOUT" << endl;
+        int weaponChoice;
+        cin >> weaponChoice;
+        if (weaponChoice == 1)
+        {
+            primaryWeapon = "M4A1";
+        }
+        if (weaponChoice == 2)
+        {
+            primaryWeapon = "AUG";
+        }
+        if (weaponChoice == 3)
+        {
+            primaryWeapon = "AWP";
+        }
+        if (weaponChoice == 4)
+        {
+            primaryWeapon = "SCOUT";
+        }
+        displayPlayerInfo();
+    }
+    void displayPlayerInfo()
+    {
+        cout << "Player Name: " << userName << endl;
+        cout << "Team: " << team << endl;
+        cout << "Health: " << health << endl;
+        cout << "Armor: " << armor << endl;
+        cout << "Primary Weapon: " << primaryWeapon << endl;
+        cout << "Secondary Weapon: " << secondaryWeapon << endl;
+    }
 };
 int main()
 {
-    player teamselection;
-    teamselection.teamSelection();
-    player p1;
+    player p1("rehan", "CT");
+    player p2("wdawda", "CT");
+    player p3("rebngfbfhan", "T");
+    player p4("reh23123342an", "T");
+    p1.buyWeapon();
+    p2.buyWeapon();
+    p3.buyWeapon();
+    p4.buyWeapon();
 }
